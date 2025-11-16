6adeebf1afef8e3d0ec50a95efd9479489a8956c
Yashwanth R <89150253+yashu1wwww@users.noreply.github.com>
2025-11-16 19:05:02 +0530

    Update shorts.py

--- shorts.py
+++ shorts.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver #10 gmails must login...
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -427,4 +427,5 @@ while True:
     except Exception as e:
         print("An error occurred:", e)
         break                
+
 
